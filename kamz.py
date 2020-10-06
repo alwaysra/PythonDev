1336751e396d21ce9470fd27d3c4b6b05517000b
alwaysra@163.com <alwaysra@163.com>
2020-10-06 17:31:56 +0800

    创建文件夹 kamz

--- kamz.py
+++ kamz.py
@@ -1,12 +1,17 @@
-import requests,random,time
+import requests,random,time,os
 
 def getmzjpg():
     req=requests.get('https://api.941ka.cn/api/mnxz/')
     wjm=int(random.randint(1,9)*time.time())
     print(wjm)
-    f=open(str(wjm)+'.jpg','wb')
+    f=open('kamz\\'+str(wjm)+'.jpg','wb')
     f.write(req.content)
     f.close()
 
-while True:
-    getmzjpg()
+if __name__ == '__main__':
+    folder = os.path.exists("kamz")
+    if not folder:
+        os.makedirs("kamz")
+    while True:
+        getmzjpg()
+
